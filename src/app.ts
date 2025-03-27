ec8920d6fa71cf6fb80a84d84291d69f35930f0e
parthivsaikia <parthivsaikia985@gmail.com>
2025-03-27 22:46:30 +0530

    add the reoutes to app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import homeRouter from "./routes/home";
+import authRouter from "./routes/authorization";
+import sessionRouter from "./routes/session";
 
 const app = express();
-
+app.use("/api/auth/*", authRouter);
 app.use(express.json());
 app.use(cors());
+app.use("/api/me", sessionRouter);
 app.use("/", homeRouter);
 
 export default app;
