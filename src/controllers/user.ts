dd4c383a2237326dd4b0b6ac264bb06724f56005
parthivsaikia <parthivsaikia985@gmail.com>
2025-03-28 23:38:18 +0530

    add create user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,13 +1,44 @@
 import { Request, Response } from "express";
 import { SignupUser } from "../utils/types";
 import bcrypt from "bcrypt";
-
 import { storeUser, getUsers } from "../actions/users";
+import prisma from "../../prisma/prisma-client";
 
-export const createUser = async (req: Request<unknown, unknown, SignupUser>, res: Response) => {
-  const { username, name, password, email, mobileNumber, state, country, role } = req.body;
-  const saltRound = 10
-  const hashedPassword =
+export const createUser = async (
+  req: Request<unknown, unknown, SignupUser>,
+  res: Response,
+) => {
+  const {
+    username,
+    name,
+    password,
+    email,
+    mobileNumber,
+    state,
+    country,
+    role,
+  } = req.body;
+  const saltRound = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRound);
+  try {
+    const user = await storeUser({
+      username,
+      name,
+      password: hashedPassword,
+      email,
+      mobileNumber,
+      state,
+      country,
+      role,
+    });
+    res.json(user);
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error
+        ? `Error in creating user : ${error.message}`
+        : `unknown error`;
+    res.status(400).json(errorMessage);
+  }
 };
 
 export const getAllUsers = async (req: Request, res: Response) => {
