92af08b74aee075c83097062bd3b764d9a2dd893
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-01 23:38:24 +0530

    add createTour controller

--- src/controllers/tour.ts
+++ src/controllers/tour.ts
@@ -1,15 +1,54 @@
-import {  Request, Response } from "express";
-import { getTours } from "../actions/tours";
+import { Request, Response } from "express";
+import { getTours, storeTour } from "../actions/tours";
+import { CreatedTour } from "../utils/types";
+import { Prisma, User } from "@prisma/client";
+import { convertBigIntToString } from "../utils/typeconverter";
 
-export const getAllTours = async (_req: Request, res: Response) => {
+export const getAllTours = async (
+  _req: Request,
+  res: Response,
+): Promise<void> => {
   try {
     const allTours = await getTours();
-    res.status(200).json(allTours);
+    const allToursWithoutBigInt = allTours.map(tour => convertBigIntToString(tour));
+
+    res.status(200).json(allToursWithoutBigInt);
   } catch (err) {
     const errorMessage =
       err instanceof Error
         ? `error in showing all tours: ${err.message}`
         : `unknown error at getting tours.`;
     res.status(500).json({ error: errorMessage });
+  }
+};
+
+export const createTour = async (
+  req: Request<unknown, unknown, CreatedTour>,
+  res: Response,
+) => {
+  const user = req.user as User;
+  const { minimumPeople, price, startDate, endDate } = req.body;
+  try {
+    const tour = await storeTour({
+      minimumPeople,
+      price: new Prisma.Decimal(price),
+      agentId: user.id,
+      startDate,
+      endDate,
+    });
+    const toBeJsonTour = {
+      minimumPeople: tour.minimumPeople,
+      price: tour.price,
+      agentId: tour.agentId.toString(),
+      startDate: tour.startDate,
+      endDate: tour.endDate,
+    };
+    return res.json(toBeJsonTour);
+  } catch (err) {
+    const errorMessage =
+      err instanceof Error
+        ? `error in creating tour: ${err.message}`
+        : `unknown error in creating user.`;
+    res.status(400).json({ error: errorMessage });
   }
 };
--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,21 +1,18 @@
 import { Router } from "express";
-import passport from "passport";
 const profileRouter = Router();
-import { Request, Response, RequestHandler } from "express";
+import { Request, Response } from "express";
 import { User } from "@prisma/client";
+import { requireJwtAuth } from "../utils/middlewares";
 
 // interface AuthenticatedRequest extends Request {
 //   user?: User; // Make user optional or handle cases where it might not exist if needed
 // }
 
-const requireJwtAuth: RequestHandler = passport.authenticate("jwt", {
-  session: false,
-}) as RequestHandler;
+// const requireJwtAuth: RequestHandler = passport.authenticate("jwt", {
+//   session: false,
+// }) as RequestHandler;
 
 profileRouter.get("/", requireJwtAuth, (req: Request, res: Response) => {
-  // If passport.authenticate('jwt', ...) succeeds, it attaches the user
-  // object (found via the JwtStrategy) to req.user.
-  // The type augmentation we did helps TypeScript know req.user is our User type.
   const user = req.user as User | undefined;
   console.log("Accessing protected profile route");
   if (!user) {
