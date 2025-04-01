6296cf91628e6e3902781671e23b667cb269778d
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-01 23:39:24 +0530

    change types and deleted welcome and profile routes

--- src/routes/profile.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getProfile } from "../controllers/profile";
-import { requireJwtAuth } from "../utils/middlewares";
-const profileRouter = Router();
-profileRouter.get("/:id", requireJwtAuth, getProfile);
-
-export default profileRouter;
