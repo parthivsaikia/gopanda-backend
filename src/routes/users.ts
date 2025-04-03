5be89114beb8cc7488c52878196c1e16ac7c42c4
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-03 20:02:07 +0530

    add isSelf middleware to get user route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { createUser, deleteUser, editUser, getAllUsers, getUser } from "../controllers/user";
-import { requireJwtAuth } from "../utils/middlewares";
+import { isSelf, requireJwtAuth } from "../utils/middlewares";
 
 const userRouter = Router();
 
@@ -8,5 +8,5 @@ userRouter.post("/", createUser);
 userRouter.get("/", getAllUsers);
 userRouter.put("/:id", editUser);
 userRouter.delete("/:id", deleteUser)
-userRouter.get("/:id", requireJwtAuth,getUser)
+userRouter.get("/:id", requireJwtAuth, isSelf, getUser)
 export default userRouter;
