5ebb538d4e90a864f37a494e5621f166626db068
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-03 20:34:54 +0530

    add routes for updating tours.

--- src/routes/tour.ts
+++ src/routes/tour.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createTour, getAllTours } from "../controllers/tour";
-import { isAgent, requireJwtAuth } from "../utils/middlewares";
+import { createTour, getAllTours, updateTour } from "../controllers/tour";
+import { isAgent, isSelf, requireJwtAuth } from "../utils/middlewares";
 const tourRouter = Router();
 
 tourRouter.get("/", getAllTours);
 tourRouter.post("/", requireJwtAuth, isAgent, createTour);
+tourRouter.put("/:id", requireJwtAuth, isSelf, updateTour);
 
 export default tourRouter;
