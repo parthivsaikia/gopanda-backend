1957aa371e013f3670f471b8e8e91e878f4d9fbb
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-09 22:45:21 +0530

    add profile.ts

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,1 +1,6 @@
 import { Router } from "express";
+import { getProfile } from "../controllers/profile";
+const profileRouter = Router();
+profileRouter.get("/:id", getProfile);
+
+export default profileRouter;
