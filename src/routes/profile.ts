a94611729ae4d6b3e55683611de60abd261022b4
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-17 23:50:49 +0530

    add prisma files

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { getProfile } from "../controllers/profile";
+import { requireJwtAuth } from "../utils/middlewares";
 const profileRouter = Router();
-profileRouter.get("/:id", getProfile);
+profileRouter.get("/:id", requireJwtAuth, getProfile);
 
 export default profileRouter;
